af4735ecc7231ac5f9d3ff50faa3c0de28bf2954
CharlesSBL <113734556+CharlesSBL@users.noreply.github.com>
2025-08-24 16:12:26 +0200

    feat: implemented react menu filter, without request ability

---
+++ src/feature/product-listing/components/filter/menuFilter/FilterItem.tsx
@@ -0,0 +1,107 @@
+import { useEffect, useRef, useState } from "react";
+
+export interface IFilterItem {
+    id: number,
+    name: string,
+    header: string,
+    optionList: string[]
+    mainOption: string
+}
+
+export interface IFilterItemRo {
+    readonly filtItem: IFilterItem
+}
+
+export function FilterItem(
+    { filtItem }: Readonly<IFilterItemRo>
+) {
+    const [showOptions, setShowOptions] = useState(false);
+    const selectedRef = useRef<HTMLDivElement>(null);
+
+
+    const [selectOpt, setSelectOpt] = useState<string>(filtItem.mainOption);
+    const [unselectOpt, setUnselectOpt] = useState<string[]>(filtItem.optionList);
+
+    useEffect(() => {
+        if (!showOptions) return;
+
+        const pageClick = (e: MouseEvent) => {
+            if (!selectedRef.current?.contains(e.target as HTMLElement)) {
+                setShowOptions(false);
+            }
+        };
+
+        document.addEventListener("click", pageClick);
+        return () => document.removeEventListener("click", pageClick);
+    }, [showOptions]);
+
+    return (
+
+        <li
+            id={`filter-${filtItem.id}`}
+            className={`menu-item filter-${filtItem.name}`}
+        >
+            <div className="menu-item-header">
+                <span>{filtItem.header}:</span>
+            </div>
+
+            <div className="options-box">
+                <div
+                    className="selected"
+                    ref={selectedRef}
+                >
+                    <button
+                        className="option selected-option"
+                        data-value={selectOpt}
+                        onClick={() => setShowOptions(!showOptions)}
+                    >
+                        <span>{selectOpt.includes("wszystkie") ? "Pokaż wszystkie" : selectOpt.replace(selectOpt.charAt(0), selectOpt.charAt(0).toLocaleUpperCase())}</span>
+                        <img src="/assets/images/arrow/arrow.svg" alt="arrow" />
+                    </button>
+                </div>
+
+                {showOptions && (
+                    <div className="non-selected">
+                        {unselectOpt.includes("wszystkie") && (
+                            <button
+                                id="option-all"
+                                className="non-selected-option"
+                                data-value="wszystkie"
+                                onClick={() => {
+                                    const newUnselectArr = unselectOpt.filter(item => !item.includes("wszystkie"));
+                                    newUnselectArr.push(selectOpt);
+                                    setUnselectOpt(newUnselectArr);
+
+                                    setSelectOpt("wszystkie");
+                                }}
+                            >
+                                <span>Wszystkie</span>
+                            </button>
+                        )}
+
+                        {unselectOpt.filter(item => !item.includes("wszystkie")).map((option: string, id) => {
+                            return (
+                                <button
+                                    key={option + id}
+                                    className="non-selected-option"
+                                    data-value={option}
+                                    onClick={() => {
+                                        const newUnselectArr = unselectOpt.filter(item => !item.includes(option));
+                                        newUnselectArr.push(selectOpt);
+                                        setUnselectOpt(newUnselectArr);
+
+                                        setSelectOpt(option);
+                                    }}
+                                >
+                                    <span>{option.replace(option.charAt(0), option.charAt(0).toLocaleUpperCase())}</span>
+                                </button>
+                            )
+                        })}
+                    </div>
+                )}
+            </div>
+        </li>
+
+
+    );
+}
--- src/feature/product-listing/components/filter/menuFilter/MenuFilter.tsx
+++ src/feature/product-listing/components/filter/menuFilter/MenuFilter.tsx
@@ -1,129 +1,40 @@
+import { FilterItem, type IFilterItem } from "./FilterItem";
+
 export default function MenuFilter() {
+    const filterItems: IFilterItem[] = [
+        {
+            id: 1,
+            name: "by",
+            header: "Sortuj po",
+            optionList: ["wszystkie", "cena", "pojemność"],
+            mainOption: "popularność"
+        }, {
+            id: 2,
+            name: "function",
+            header: "Funkcje",
+            optionList: ["Drzwi AddWash", "Panel AI Control", "Silnik inwerterowy", "Wyświetlacz elektroniczny"],
+            mainOption: "wszystkie"
+        }, {
+            id: 3,
+            name: "by",
+            header: "Sortuj po",
+            optionList: ["A", "B", "C", "D"],
+            mainOption: "wszystkie"
+        }, {
+            id: 4,
+            name: "by",
+            header: "Sortuj po",
+            optionList: ["9kg", "8kg", "10.5kg"],
+            mainOption: "wszystkie"
+        }
+    ];
+
+
     return (
         <ul className='filter-menu'>
-            <li id='filter-1' className='menu-item filter-by'>
-                <div className="menu-item-header">
-                    <span>Sortuj po:</span>
-                </div>
-
-                <div className="options-box">
-                    <div className='selected'>
-                        <div className="option selected-option" data-value="popularność">
-                            <span>Popularność</span>
-                            <img src="/" alt="arrow" />
-                        </div>
-                    </div>
-
-                    {/* <div className='non-selected'>
-                  <div className="option" data-value="cena">
-                    <span>Cena</span>
-                    <img src="/" alt="arrow" />
-                  </div>
-                  <div className="option" data-value="pojemność">
-                    <span>Pojemność</span>
-                    <img src="/" alt="arrow" />
-                  </div>
-                </div> */}
-                </div>
-            </li>
-
-            <li id='filter-2' className='menu-item filter-function'>
-                <div className="menu-item-header">
-                    <span>Funkcje:</span>
-                </div>
-                <div className="options-box">
-                    <div className='selected'>
-                        <div className="option selected-option" data-value="pokaż-wszystkie">
-                            <span>Pokaż wszystkie</span>
-                            <img src="/" alt="arrow" />
-                        </div>
-                    </div>
-                    {/* <div className='non-selected'>
-                <div className="option" data-value="drzwi-addwash">
-                <span>Drzwi AddWash™</span>
-                <img src="/" alt="arrow" />
-                </div>
-                <div className="option" data-value="panel-ai-control">
-                <span>Panel AI Control</span>
-                <img src="/" alt="arrow" />
-                </div>
-                <div className="option" data-value="silnik-inwerterowy">
-                <span>Silnik inwerterowy</span>
-                <img src="/" alt="arrow" />
-                </div>
-                <div className="option" data-value="wyświetlacz-elektroniczny">
-                <span>Wyświetlacz elektroniczny</span>
-                <img src="/" alt="arrow" />
-                </div>
-              </div> */}
-                </div>
-            </li>
-
-            <li id='filter-3' className='menu-item filter-energy'>
-                <div className="menu-item-header">
-                    <span>Klasa energetyczna:</span>
-                </div>
-                <div className="options-box">
-                    <div className='selected'>
-                        <div className="option selected-option" data-value="pokaż-wszystkie">
-                            <span>Pokaż wszystkie</span>
-                            <img src="/" alt="arrow" />
-                        </div>
-                    </div>
-                    {/* <div className='non-selected'>
-                <div className="option" data-value="a">
-                <span>A</span>
-                <img src="/" alt="arrow" />
-                </div>
-                <div className="option" data-value="b">
-                <span>B</span>
-                <img src="/" alt="arrow" />
-                </div>
-                <div className="option" data-value="c">
-                <span>C</span>
-                <img src="/" alt="arrow" />
-                </div>
-                <div className="option" data-value="d">
-                <span>D</span>
-                <img src="/" alt="arrow" />
-                </div>
-                <div className="option" data-value="f">
-                <span>F</span>
-                <img src="/" alt="arrow" />
-                </div>
-              </div> */}
-                </div>
-            </li>
-
-            <li id='filter-4' className='menu-item filter-capacity'>
-                <div className="menu-item-header">
-                    <span>Pojemność:</span>
-                </div>
-                <div className="options-box">
-                    <div className='selected'>
-                        <div className="option selected-option" data-value="pokaż-wszystkie">
-                            <span>Pokaż wszystkie</span>
-                            <img src="/" alt="arrow" />
-                        </div>
-                    </div>
-                    {/* <div className='non-selected'>
-                  <div className="option" data-value="9kg">
-                    <span>9kg</span>
-                    <img src="/" alt="arrow" />
-                  </div>
-                  <div className="option" data-value="8kg">
-                    <span>8kg</span>
-                    <img src="/" alt="arrow" />
-                  </div>
-                  <div className="option" data-value="10.5kg">
-                    <span>10.5kg</span>
-                    <img src="/" alt="arrow" />
-                  </div>
-                </div> */}
-                </div>
-            </li>
+            {filterItems.map((item) => (
+                <FilterItem key={item.id} filtItem={item} />
+            ))}
         </ul>
-
-
-    );
+    )
 }
